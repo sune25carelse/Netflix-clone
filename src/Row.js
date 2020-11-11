6c4b060621cc2806fd6a98465327a983dc2b46df
Sune Carelse <Sune.Carelse@hq.takealot.com>
2020-11-11 22:47:32 +0200

    created app with api , rows&movies

---
+++ src/Row.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect }  from 'react';
+import axios from './axios';
+
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results)
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    console.log(movies);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row__posters">
+                {/* several row__poster(s) */}
+
+                {movies.map(movie => (
+                    <img src={movie.poster_path} alt={movie.name}/>
+                ))}
+            </div>
+        
+            {/* container -> posters */}
+        </div>
+  );
+}
+
+export default Row;
