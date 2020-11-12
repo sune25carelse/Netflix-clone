48780f7f523df2d3165ec3e8a5f6d0fbfa177577
Sune Carelse <Sune.Carelse@hq.takealot.com>
2020-11-12 23:04:44 +0200

    style banner  title/descrip/content

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './requests';
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -30,12 +31,16 @@ function Banner() {
           backgroundPosition:"center center",
       }}
     > 
-      <div className="banner__container">
+      <div className="banner__contents">
         {/* title */}
-        <h1>
-            {movie?.title || movie?.name || movie?.original_name}
-        </h1>
-        {/* div > 2 buttons */}
+        <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+
+        <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+        </div>
+
+        <h1 className="banner__description">{movie?.overview}</h1>
         {/* description */}    
       </div>  
     </header>
