c80b407b5c7dede6f4381e5e639f3bed3e57eb26
Sune Carelse <Sune.Carelse@hq.takealot.com>
2020-11-14 21:56:47 +0200

    navbar and banner styled

--- src/Banner.js
+++ src/Banner.js
@@ -45,7 +45,9 @@ function Banner() {
         </div>
 
         <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
-      </div>  
+      </div>
+
+      <div className="banner--fadeBottom" />  
     </header>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import './Nav.css';
+
+function Nav() {
+  const [show, handleShow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else handleShow(false);
+    });
+    return () => {
+        window.removeEventListener("scroll");
+    };
+  }, []);
+    
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+        <img 
+          classNamwe="nav__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0f/"
+          alt="Netflix Logo"
+        />
+
+        <img 
+          classNamwe="nav__avatar"
+          src="https://pbs.twig.com/profile_images/124011999041155"
+          alt="Netflix Logo"
+        />
+    </div>
+  );
+}
+
+export default Nav;
